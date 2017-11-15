b023af5f0f1b7e50a70f40a2bd69df884b83b41e
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-15 14:12:37 +0000

    Wed Nov 15 14:12:37 GMT 2017

--- src/main/kotlin/befaster/solutions/Item.kt
+++ src/main/kotlin/befaster/solutions/Item.kt
@@ -6,33 +6,76 @@ object A : Item(50)
 object B : Item(30)
 object C : Item(20)
 object D : Item(15)
+object E : Item(40)
 object Invalid : Item(-1)
 
 typealias Basket = List<Item>
 typealias Price = Int
 
-class Checkout(basket: Basket) {
-    val total: Price = if(basket.contains(Invalid)) -1 else
-            basket.groupBy { it }.map {
-                when {
-                    it.key is A -> bulkPrice(it.key, 3, it.value.size, 130)
-                    it.key is B -> bulkPrice(it.key, 2, it.value.size, 45)
-                    else -> it.value.sumBy { it.price }
-                }
-            }.sum()
+interface Deal {
+    val howManyYouNeedToBuy: Int
+}
+data class NAtPriceOf(val item: Item, override val howManyYouNeedToBuy: Int, val priceOf: Price): Deal {
+    fun reducedPrice(numberOfItems: Int): Price {
+        return (numberOfItems % howManyYouNeedToBuy) * item.price + (numberOfItems / howManyYouNeedToBuy) * priceOf
+    }
+}
 
-    private fun bulkPrice(item: Item, itemsOnOffer: Int, numberOfItems: Int, pricePerBulkItems: Int): Int {
-        return if (numberOfItems % itemsOnOffer == 0) reducedPrice(numberOfItems, pricePerBulkItems, itemsOnOffer)
-        else {
-            reducedPrice(numberOfItems, pricePerBulkItems, itemsOnOffer) + (numberOfItems.mod(itemsOnOffer)) * item.price
-        }
+data class BuyItemGetItemFree(
+        val whatYouGetForFree: Item,
+        val whatYouNeedToBuy: Item,
+        override val howManyYouNeedToBuy: Int,
+        val howManyYouGetForFree: Int) : Deal {
+
+    fun reducedPrice(howManyYouHave: Int, howManyOfOther: Int): Price {
+        val numberOfDiscounts = howManyOfOther / howManyYouNeedToBuy
+        val result = ((howManyYouHave - numberOfDiscounts) * whatYouGetForFree.price)
+        return Math.max(0, result)
     }
 
-    private fun reducedPrice(numberOfItems: Int, bulkPrice: Int, itemsOnOffer: Int) = bulkPrice * (numberOfItems / itemsOnOffer)
+}
+object NoDeal: Deal {
+    override val howManyYouNeedToBuy: Int = 1
+}
 
+val offers = mapOf(
+        A to listOf(NAtPriceOf(A, 3,130), NAtPriceOf(A, 5, 200)),
+        B to listOf(BuyItemGetItemFree(B, E, 2, 1), NAtPriceOf(B, 2, 45))
+)
+
+class Checkout(basket: Basket) {
     companion object {
         fun priceFor(items: String): Price = Checkout(Scanner().getBasketFor(items)).total
     }
+
+    val total: Price = if(basket.contains(Invalid)) -1 else {
+        val groupedItems = basket.groupBy { it }
+        val priceForItems = groupedItems.map { group ->
+            val deals = offers[group.key] ?: listOf(NoDeal)
+            val reducedPrice = getPriceFor(group.key, group.value.size, deals, groupedItems)
+            reducedPrice.min()!!
+        }.sum()
+        priceForItems
+    }
+
+    fun getPriceFor(item: Item, numberOfItems: Int, deals: List<Deal>, group: Map<Item, List<Item>>): List<Int> {
+        fun go(list: List<Item>): List<Int> {
+            return when {
+                list.isEmpty() -> listOf(0)
+                else -> (listOf(numberOfItems) + deals.map { it.howManyYouNeedToBuy }).flatMap { howMany ->
+                    deals.map {
+                        when (it) {
+                            is NAtPriceOf -> it.reducedPrice(howMany) + go(list.drop(howMany)).min()!!
+                            is BuyItemGetItemFree -> it.reducedPrice(list.size, group[it.whatYouNeedToBuy].orEmpty().size) + go(list.drop(it.howManyYouNeedToBuy)).min()!!
+                            else -> item.price * howMany + go(list.drop(howMany)).min()!!
+                        }
+                    }
+                }
+            }
+        }
+        return go(group[item].orEmpty())
+    }
+
 }
 
 class Scanner {
@@ -42,6 +85,7 @@ class Scanner {
             'B' -> B
             'C' -> C
             'D' -> D
+            'E' -> E
             else -> Invalid
         }
     }
--- src/test/kotlin/befaster/solutions/CheckoutTest.kt
+++ src/test/kotlin/befaster/solutions/CheckoutTest.kt
@@ -11,6 +11,7 @@ class CheckoutTest {
         expect that B.price isEqualTo 30
         expect that C.price isEqualTo 20
         expect that D.price isEqualTo 15
+        expect that E.price isEqualTo 40
     }
 
 
@@ -36,12 +37,14 @@ class CheckoutTest {
 
     @Test
     fun `it should parse a valid list of items and return total`(){
-        expect that Checkout.priceFor("ABCDABC") isEqualTo 200
+
         expect that Checkout.priceFor("B") isEqualTo B.price
         expect that Checkout.priceFor("A") isEqualTo A.price
         expect that Checkout.priceFor("B") isEqualTo B.price
         expect that Checkout.priceFor("C") isEqualTo C.price
         expect that Checkout.priceFor("D") isEqualTo D.price
+        expect that Checkout.priceFor("E") isEqualTo E.price
+        expect that Checkout.priceFor("ABCDABC") isEqualTo 200
     }
 
     @Test
@@ -49,6 +52,34 @@ class CheckoutTest {
         val items = "ABCDABFOO"
 
         expect that Checkout.priceFor(items) isEqualTo -1
+    }
+
+    @Test
+    fun `it should implement multiple offers for same item`(){
+
+        expect that Checkout(listOf(A, A, A)).total isEqualTo 130
+        expect that Checkout(listOf(A, A, A, A)).total isEqualTo 130 + A.price
+        expect that Checkout(listOf(A, A, A, A, A)).total isEqualTo 200
+        expect that Checkout(listOf(A, A, A, A, A, A)).total isEqualTo Math.min(200 + A.price, (130 * 2) + A.price)
+
+        expect that Checkout.priceFor("AAAAAAAA") isEqualTo 330
+        expect that Checkout.priceFor("AAAAAEEBAAABB") isEqualTo 455
+        expect that Checkout.priceFor("AAAAAAAAA") isEqualTo 390
+    }
+
+    @Test
+    fun `it should implement buy one item to get other items for free`(){
+
+        expect that Checkout(listOf(E, E, B)).total isEqualTo 2 * E.price
+        expect that Checkout(listOf(E, E, C)).total isEqualTo 2 * E.price + C.price
+        expect that Checkout(listOf(E, E, B, B)).total isEqualTo 2 * E.price + B.price
+        expect that Checkout(listOf(E, E, B, B)).total isEqualTo Math.min(2 * E.price + B.price, 2 * E.price + B.price)
+
+    }
+
+    @Test
+    fun buyItemGetItemFreeTest(){
+
     }
 
 }
